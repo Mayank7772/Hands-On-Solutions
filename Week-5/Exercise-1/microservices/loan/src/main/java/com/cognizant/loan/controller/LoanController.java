8be46349363c2f82b5a5e6de7efa249960866559
Mayank7772 <mayankrajput82258@gmail.com>
2025-08-03 16:48:40 +0530

    updation week-5 hands-on

--- Week-5/Exercise-1/microservices/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
+++ Week-5/Exercise-1/microservices/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -1,5 +1,13 @@
 package com.cognizant.loan.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/loans")
 public class LoanController {
 
+    @GetMapping("/{number}")
+    public Loan getLoanDetails(@PathVariable String number) {
+        return new Loan(number, "car", 400000, 3258, 18);
+    }
 }
